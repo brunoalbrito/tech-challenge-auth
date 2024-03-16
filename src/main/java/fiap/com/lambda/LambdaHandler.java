fa41f692904b501ea7313820f8af473d90d83c43
brunoalbrito <brunoalbuquerquebrito@outlook.com>
2024-03-16 14:33:50 -0300

    first commit

---
+++ src/main/java/fiap/com/lambda/LambdaHandler.java
@@ -0,0 +1,19 @@
+package fiap.com.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
+import jakarta.inject.Named;
+
+@Named("handler")
+public class LambdaHandler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
+
+    @Override
+    public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
+        APIGatewayV2HTTPResponse response = new APIGatewayV2HTTPResponse();
+        response.setBody(event.getBody());
+
+        return response;
+    }
+}
---
+++ src/test/java/fiap/com/lambda/LambdaHandlerTest.java
@@ -0,0 +1,26 @@
+package fiap.com.lambda;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+
+@QuarkusTest
+class LambdaHandlerTest {
+    @Test
+    void testSimpleLambdaSuccess() throws Exception {
+        // you test your lambdas by invoking on http://localhost:8081
+        // this works in dev mode too
+        given()
+                .contentType("application/json")
+                .accept("application/json")
+                .body("{\"body\": \"Hello Stu\"}")
+                .when()
+                .post()
+                .then()
+                .statusCode(200)
+                .body(containsString("Hello Stu"));
+    }
+
+}
