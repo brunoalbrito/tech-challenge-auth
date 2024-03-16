c79ac9fc76f77a847d6d513f5f5981f0bd00f541
brunoalbrito <brunoalbuquerquebrito@outlook.com>
2024-03-16 18:58:05 -0300

    New event test

--- src/main/java/fiap/com/lambda/LambdaHandler.java
+++ src/main/java/fiap/com/lambda/LambdaHandler.java
@@ -2,18 +2,17 @@ package fiap.com.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import jakarta.inject.Named;
 
 @Named("handler")
-public class LambdaHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+public class LambdaHandler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
 
     @Override
-    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
-        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
-        response.setStatusCode(200);
-        response.setBody(event.getBody());
+    public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
+        APIGatewayV2HTTPResponse response = new APIGatewayV2HTTPResponse();
+        response.setBody(event.getBody() + "new event test");
 
         return response;
     }
